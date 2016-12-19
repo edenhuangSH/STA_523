dfb55c8bc4427e76fde4e25ebc97d9916daf60ce
Huang Huang <huang.huang1@duke.edu>
2016-12-19 16:10:10 -0500

    na

--- preprocessing.R
+++ preprocessing.R
@@ -72,20 +72,20 @@ load(file.path('data', 'job_train.RData'))
 # --------------------------------------------------------------------------
 # Obtain relative job title of each category
 
-cat <- cbind(job_train_lda$results.jobtitle, job_train_lda$results.company,
+category <- cbind(job_train$results.jobtitle, job_train$results.company,
              topics(lda_model) %>%
                matrix(ncol = 1)) %>% 
   data.frame(stringsAsFactors = FALSE) %>% 
   setNames(c("JobTitle", "Company", "Cluster"))
 
 for (i in 1:3) {
-  assign(paste0("title", i), cat[which(cat$Cluster == i), 1] %>%
+  assign(paste0("title", i), category[which(category$Cluster == i), 1] %>%
            table() %>%
            sort(decreasing = TRUE) %>%
            head(n = 3L) %>%
            as.data.frame() %>%
            setNames(c("JobTitle", "Frequents")))
-  assign(paste0("company", i), cat[which(cat$Cluster == i), 2] %>%
+  assign(paste0("company", i), category[which(category$Cluster == i), 2] %>%
            table() %>%
            sort(decreasing = TRUE) %>%
            head(n = 3L) %>%
